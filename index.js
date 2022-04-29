3e49dc62884685bc646411cdadc1b1b2b52fc4de
ajpicc3734 <ajpicc3734@gmail.com>
2022-04-29 15:22:13 -0400

    added user input data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
 
 //console.log(inquirer);
 
@@ -19,40 +20,58 @@ const questions = () => {
       name: "description",
       message: "Provide a description of the project.",
     },
+    //installation section
     {
       type: "input",
       name: "installation",
       message: "Enter installation instructions for the peoject",
     },
+    //usage section
     {
       type: "input",
       name: "usage",
       message: "Enter usage information for the peoject",
     },
+    //contribution section
     {
       type: "input",
       name: "contruibution",
       message: "Enter contribution guidelines for the peoject",
     },
+    //tests section
     {
       type: "input",
       name: "test",
       message: "Enter test instructions for the peoject",
     },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Choose licens type",
+      choices: [
+        "Open Source Software License",
+        "Public Domain License",
+        "Permissive License",
+        "Restrictive Licenses",
+        "LGPL",
+        "Proprietary Software License",
+      ],
+    },
+    //questions section
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub username",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address",
+    },
   ]);
 };
 
 questions().then((answers) => console.log(answers));
-
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       name: "name",
-//       message: "What is your name?",
-//     },
-//   ])
-//   .then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
